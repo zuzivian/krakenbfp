8d635c5735d3721a2d89cd7ad58fe886bfad7a53
Nathaniel Wong <natmikewong@gmail.com>
2016-08-31 18:41:37 +0800

    added spew_nonsense()
    - Added the spew_nonsense() function, which selects from a list of
    random messages
    
    - cleaned up some code and added more comments

--- index.php
+++ index.php
@@ -1,67 +1,112 @@
 <?php
 
 /*
-* This file is part of GeeksWeb Bot (GWB).
-*
-* GeeksWeb Bot (GWB) is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 3
-* as published by the Free Software Foundation.
-* 
-* GeeksWeb Bot (GWB) is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
-*
-* Author(s):
-*
-* © 2015 Kasra Madadipouya <kasra@madadipouya.com>
-*
+--------------------------------------
+KRAKEN: A BFP BOT
+--------------------------------------
 */
+
+// DO NOT EDIT
 require 'vendor/autoload.php';
 
 $client = new Zelenin\Telegram\Bot\Api('252926927:AAE7Fa8RTYW2D-RVYJ1B6_A77QZg5vWLJNg'); // Set your access token
 $update = json_decode(file_get_contents('php://input'));
 
-//your app
-try {
+// DO NOT EDIT THE ABOVE
 
-	// message processing
-	$text = $update->message->text;
-	$words = explode(" ", $text);
-	
-    if ($words[0] == '/add' || $words[0] == '/add@bfpbot')
+
+// --------------------------------------
+/* FUNCTIONS */
+// Beware when editing please.
+
+
+// Spews any one of the available random messages.
+function spew_nonsense() 
+{
+	$r = rand(1,2);
+    switch($r) 
     {
-		$msg = "Feature unavailable. Sorry folks.";
-    }
-    else if ($words[0] == '/help' || $words[0] == '/add@bfpbot')
-    {
-		$msg = "List of commands :\n /add -> adds to Kraken's existing repertoire  \n /help -> Shows list of available commands";
-    }
-    else 
-    {
-	     $randnum = rand(1,300);
-         switch($randnum) {
-           case 1:
-               $msg = "Grandpa nyat is watching.";
-               break;
-           case 2:
-               $msg = "bukkake over the whole school population";
-               break;
-         }
-    }
-    
-	if ($msg) {
-		$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-		$response = $client->sendMessage([
-      		'chat_id' => $update->message->chat->id,
-			'text' => $msg
- 		]);
- 	}
-    
-
-} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-
-    //echo error message ot log it
-    //echo $e->getMessage();
-
+		case 1:
+        	$msg = "Grandpa nyat is watching";
+            break;
+        case 2:
+			$msg = "bukkake over the whole school population";
+			break;
+	}
 }
+
+/* END FUNCTIONS */
+// -------------------------------------
+
+
+
+//   -------------------
+//   MESSAGE PROCESSING
+//   -------------------
+// This is where the bot's logic resides.
+
+
+// Takes incoming message and breaks it down into an array of strings, one word per string
+// Usage: '$words[0]' means the first word in the message
+$words = explode(" ", $update->message->text);
+$words[0] = $cmd;
+
+// The Bot now decides, based on the ruleset, on an appropriate response, or to keep quiet.
+
+// LIST OF EXISTING COMMANDS
+// add - adds to Kraken's existing repertoire (broken)
+// help - shows list of available commands
+// nonsense - encourages the Kraken to spew some nonsense 
+
+if ($cmd == '/add' || $cmd == '/add@bfpbot')
+{
+	// The add message function is still under construction.
+	$msg = "Feature unavailable. Sorry folks.";
+}
+
+else if ($cmd == '/help' || $cmd == '/help@bfpbot') 
+{
+	// Sends a help message
+	$msg = "List of commands :\n /add -> adds to Kraken's existing repertoire  \n /help -> Shows list of available commands
+	\n /nonsense - encourages the Kraken to spew some nonsense ";
+}
+
+else if ($cmd == '/nonsense' || $cmd == '/nonsense@bfpbot') 
+{
+	// Spew nonsense.
+	spew_nonsense;
+}
+
+else    
+{
+	// If all else fails, rolls a dice to see if bot should keep quiet or spew nonsense.
+	$randnum = rand(1,100)
+	if ($randnum == 1) 
+	{
+		spew_nonsense;
+	}
+}
+
+
+
+// ----------------
+// SENDING MESSAGE
+// ----------------
+
+// If an appropriate message response exists, send it via the Telegram API to the chat. 
+// Do not edit!   
+if ($msg) {
+	// Sends the "Typing..." action to Telegram
+	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+	
+	// Now sends the approriate message
+	$response = $client->sendMessage([
+     	'chat_id' => $update->message->chat->id,
+		'text' => $msg
+ 	]);
+ }
+    
+
+// -------------------------    
+// END OF SCRIPT. WOOHOO!
+// -------------------------
