5469bb91d1ac40aa20702908709ea4b0a42337d6
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 17:36:29 +0800

    updated select_random_msg()

--- db.php
+++ db.php
@@ -11,28 +11,16 @@ $db = substr($url["path"], 1);
 function select_random_msg() 
 {
 	$conn = new mysqli($server, $username, $password, $db);
-	$sql = "SELECT response FROM kraken_msg LIMIT 1";
+	$sql = "SELECT response FROM kraken_msg ORDER BY RAND() LIMIT 1";
 	if ($result = $conn->query($sql)) {
 		$row = $result->fetch_assoc();
 	}
-	echo $row['response'];
+	return $row['response'];
 }
 
-select_random_msg();
+echo select_random_msg();
 
-// function select_random_msg() {
-// 	$query = "SELECT response FROM kraken_msg ORDER BY RAND() LIMIT 1";
-// 	if($database->num_rows( $query ) > 0)
-// 	{
-//     	list($msg) = $database->get_row($query);
-//     	return $msg[];
-// 	}	
-// 	else 
-// 	{
-// 		return null;
-// 	}
-// 	
-// }
+
 
 ?>
 
