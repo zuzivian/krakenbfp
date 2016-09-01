f3fb0d3cbf7c397db8f76c9aa997d6915c6f722b
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 17:01:46 +0800

    db debug

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
 echo "hi2";
-print_r $url;
 
 // $conn = new mysqli($server, $username, $password, $db);
 
