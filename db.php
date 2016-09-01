3004ef30925bf53db3808077b3eed5091caacc2d
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 17:12:26 +0800

    removed test code

--- db.php
+++ db.php
@@ -10,12 +10,6 @@ echo "hi4";
 
 $conn = new mysqli($server, $username, $password, $db);
 
-$sql = "SELECT response FROM kraken_msg LIMIT 1";
-if ($result = $conn->query($sql)) {
-	$row = $result->fetch_assoc();
-}
-
-echo $row['response'];
 
 function select_random_msg() {
 	$sql = "SELECT response FROM kraken_msg ORDER BY RAND() LIMIT 1";
