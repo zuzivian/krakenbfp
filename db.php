27abd39fa445ca1f5ac933793cf84047f258b532
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 17:34:11 +0800

    db debug

--- db.php
+++ db.php
@@ -8,16 +8,16 @@ $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 
-function select_random_msg() 
-{
+//function select_random_msg() 
+//{
 	$sql = "SELECT response FROM kraken_msg LIMIT 1";
 	if ($result = $conn->query($sql)) {
 		$row = $result->fetch_assoc();
 	}
 	echo $row['response'];
-}
+//}
 
-select_random_msg();
+//select_random_msg();
 
 // function select_random_msg() {
 // 	$query = "SELECT response FROM kraken_msg ORDER BY RAND() LIMIT 1";
