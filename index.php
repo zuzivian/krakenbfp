723bae4c5afba6965e31b09678bd60c4e1c389c4
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 16:44:14 +0800

    Try in main

--- index.php
+++ index.php
@@ -236,6 +236,27 @@ echo spew_nonsense();
 // END OF SCRIPT. WOOHOO!
 // -------------------------
 
+try {
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
+
+	$sql = "SELECT response FROM kraken_msg LIMIT 1";
+	if ($result = $mysqli->query($sql)) {
+		$msg = $result->fetch_assoc();
+	}
+
+	print_r $msg;
+} 
+catch 
+{
+	echo "failed"; 
+}
 
 
 
