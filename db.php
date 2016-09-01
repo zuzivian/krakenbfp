a75b93543895ace02a6a5126987253601784c673
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 17:04:21 +0800

    db debug

--- db.php
+++ db.php
@@ -6,15 +6,15 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-echo "hi3";
+echo "hi4";
 
 $conn = new mysqli($server, $username, $password, $db);
 
-// $sql = "SELECT response FROM kraken_msg LIMIT 1";
-// if ($result = $conn->query($sql)) {
-// 	$msg = $result->fetch_assoc();
-// }
-// 
+$sql = "SELECT response FROM kraken_msg LIMIT 1";
+if ($result = $conn->query($sql)) {
+	$msg = $result->fetch_assoc();
+}
+
 // print_r $msg;
 
 ?>
