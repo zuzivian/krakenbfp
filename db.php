5484957e913b5489360f58e3c059ceff9c42270f
Nathaniel Wong <natmikewong@gmail.com>
2016-09-01 17:48:13 +0800

    Updated get_random_mesaage

--- db.php
+++ db.php
@@ -6,11 +6,11 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-
-
-function select_random_msg() 
-{
 	$conn = new mysqli($server, $username, $password, $db);
+
+function select_random_msg($conn) 
+{
+
 	$sql = "SELECT response FROM kraken_msg ORDER BY RAND() LIMIT 1";
 	if ($result = $conn->query($sql)) {
 		$row = $result->fetch_assoc();
@@ -18,7 +18,7 @@ function select_random_msg()
 	return $row['response'];
 }
 
-echo select_random_msg();
+echo select_random_msg($conn);
 
 
 
