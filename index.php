56db05a2a52421e262414b3498a16a8e6a17ef9e
Nathaniel Wong <natmikewong@gmail.com>
2016-12-03 01:48:29 -0600

    turning off debug mode

--- index.php
+++ index.php
@@ -10,18 +10,7 @@ Version: 0.3-alpha
 
 // DO NOT EDIT
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 require 'vendor/autoload.php';
-require_once 'utils.php';
-require_once 'commands.php';
-require_once 'config.php';
-require_once 'user.php';
-
-
-
-// Takes incoming message and breaks it down into an array of strings, $words
 
 if (!$mode) {
 	$client = new Zelenin\Telegram\Bot\Api('252926927:AAE7Fa8RTYW2D-RVYJ1B6_A77QZg5vWLJNg');
@@ -31,10 +20,23 @@ if (!$mode) {
 	$chat_type = $update->message->chat->type;
 }
 else {
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
 	$text = "Y'all are so damn rude";
 	$user_submit = new User(1234,'Shen Ying','Gan','shenying');
 	$chat_type = 'private';
 }
+
+require_once 'utils.php';
+require_once 'commands.php';
+require_once 'config.php';
+require_once 'user.php';
+
+
+
+// Takes incoming message and breaks it down into an array of strings, $words
+
+
 
 $words = explode(" ", $text);
 $cmd = $words[0];
@@ -101,7 +103,7 @@ if ($msg && !$mode) {
  	]);	
 }
 else {
-	echo "Kraken 0.3-alpha \n\nMessage:";
+	echo "Kraken 0.3-alpha // Message:";
 	echo $msg;
 }
 
