d1ab647c0a7bb333f1201b39a070ea10ef2e11a0
Nathaniel Wong <natmikewong@gmail.com>
2016-12-03 02:21:12 -0600

    debug mode enabled

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 // Program Mode (0 = production, 1 = debug)
-$mode = 0;
+$mode = 1;
 
 
 
 // Chances (in percentage up to 2 decimal places)
-$chance_user_phrase = 40;
-$chance_phrase = 10;
-$chance_user = 2;
-$chance_random = 0.5;
+$chance_user_phrase = 0;
+$chance_phrase = 0;
+$chance_user = 0;
+$chance_random = 100;
