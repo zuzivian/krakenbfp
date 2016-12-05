8e0bc124159bbee88d868b06655278aa284735b8
Nathaniel Wong <natmikewong@gmail.com>
2016-12-05 15:03:23 -0600

    bugfix

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 require_once 'database.php';
 require_once 'utils.php';
 require_once 'commands.php';
 require_once 'config.php';
 require_once 'user.php';
-
 
 $db = new Database;
 
