8aa4208e19c30c774b0629741a170d4f295548bd
Nathaniel Wong <natmikewong@gmail.com>
2016-12-05 15:36:27 -0600

    changed chance configs

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ $mode = 0;
 
 // Chances (in percentage up to 2 decimal places)
 $chance_user_phrase = 0;
-$chance_phrase = 0;
-$chance_user = 0;
-$chance_random = 100;
+$chance_phrase = 40;
+$chance_user = 20;
+$chance_random = 1.5;
