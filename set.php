57eb1279e4ce1b9896fa256255b00e62d65623ac
Nathaniel Wong <natmikewong@gmail.com>
2017-08-18 23:10:08 +0800

    Mispell

--- set.php
+++ set.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '252926927:AAEO2sCissM_BB-FOll_X2mmIsheGTu7OCs';
-$hook_url = 'https://krakenbfp.heroku.com/';
+$hook_url = 'https://krakenbfp.herokuapp.com/';
 
 try {
     // Create Telegram API object
